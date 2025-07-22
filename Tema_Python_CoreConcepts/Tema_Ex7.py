27ea080f7f192b1096aa8ae1052bc4757ef4bfd5
MilitaruEduardDorin <ed.militaru@gmail.com>
2025-07-22 20:28:21 +0300

    Adaugat si exercitiul 7

--- Tema_Python_CoreConcepts/Tema_Ex7.py
+++ Tema_Python_CoreConcepts/Tema_Ex7.py
@@ -21,11 +21,36 @@ def url_response_time (url="https://www.google.com/"):
     durata_ms=(end - start) * 1000
     return durata_ms
 
-url=sys.argv[1]
+# url=sys.argv[1]
 
-print(f"Timp raspuns pt {url} : ", url_response_time(url))
+# print(f"Timp raspuns pt {url} : ", url_response_time(url))
 
 # 2. Adaugati încă o funcție ce primește doi parametrii (un url si un numar de repetari): 
-    # ○ Funcția va face într-un for sau while un număr de cal-uri către url și va memora timpurile de răspuns într-o listă (folosind prima metoda) 
+
+def functie_2 (url,repetari):
+    # ○ Funcția va face într-un for sau while un număr de cal-uri către url și va memora timpurile de răspuns într-o listă (folosind prima metoda)
+    call=0
+    list_durata =[]
+    while call < repetari :
+        call +=1
+        durata = url_response_time(url)
+        if durata is not None:
+            list_durata.append(durata)
+        else:
+            print(f'Eroare cerere HTTTp la incercarea {call} din {repetari}')
+        time.sleep(2)
     # ○ Funcția va face o medie și va întoarce într-un tuplu 3 valori în următoarea ordine: (min, media, max) 
+
+    if list_durata:
+        maxim = max(list_durata)
+        minim = min(list_durata)
+        medie = sum(list_durata) / len(list_durata)
+        return (minim, medie , maxim)
+    else:
+        return(None, None, None)
+
+rezultat = functie_2 (url="https://www.google.com/",repetari=5)
+print(f'\nTimp minim: {rezultat[0]:.2f} ms')
+print(f'\nTimp mediu: {rezultat[1]:.2f} ms')
+print(f'\nTimp maxim: {rezultat[2]:.2f} ms')
     # ○ Rulați de cateva ori funcția cu cateva url-uri si afisati rezultatele
